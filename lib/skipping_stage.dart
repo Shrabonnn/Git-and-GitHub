b5534cc0b8fb00f7711d7cde674c95b1d93241eb
shrabonnn <shrabonewmc009@gmail.com>
2026-08-11 11:07:34 +0600

    restore skipping file

---
+++ lib/skipping_stage.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/cupertino.dart';
+
+class SkippingStage extends StatelessWidget {
+  const SkippingStage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    
+    return Column(
+      
+      children: [
+        
+        Text("Hello Skipping Stage"),
+        Text("Hello Second line"),
+        Text("Hello third line"),
+        Text("Hello third line"),
+        Text("Hello third line"),
+
+        Text("Line after adding default file"),
+
+
+
+
+      ],
+    );
+  }
+}
