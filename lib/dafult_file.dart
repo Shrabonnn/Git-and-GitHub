7662b55da6a49e81c39e98e9f101f773f557d9bc
shrabonnn <shrabonewmc009@gmail.com>
2026-08-11 10:14:24 +0600

    add default

---
+++ lib/dafult_file.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class DafultFile extends StatelessWidget {
+  const DafultFile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+
+      children: [
+
+
+      ],
+    );
+  }
+}
